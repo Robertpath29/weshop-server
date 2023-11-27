9f7fef570c967962febf5b03f16d19badd5709c7
Yevhenii Shapoval <RobertPath29@gmail.com>
2023-11-27 18:46:35 +0200

    update validates user, rubocop check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
       render json: { status: 'success', message: 'User created' }
     else
-      render json: { status: 'error', message: @user.errors.full_messages.join(', ') },
-             status: :unprocessable_entity
+      render json: { status: 'error', message: @user.errors.full_messages.join(', '), warning: @user.errors }
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  before_validation :downcase_email
+
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
 
   validate :correct_email, on: :create
+  validate :name_not_integer, on: :create
 
   private
 
@@ -12,5 +17,13 @@ class User < ApplicationRecord
     return if address.valid? && address.valid_mx?
 
     errors.add :email, 'no correct'
+  end
+
+  def name_not_integer
+    errors.add(:name, 'should not be a number') if name.present? && name.match?(/\A\d+\z/)
+  end
+
+  def downcase_email
+    self.email = email.downcase if email.present?
   end
 end
